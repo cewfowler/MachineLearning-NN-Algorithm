bd8cd942b4069f60bcca105385e342badb56dd14
Chris F <cewfowler@gmail.com>
2021-01-04 21:04:16 -0600

    Calculate sigmoid gradient

--- ex4/sigmoidGradient.m
+++ ex4/sigmoidGradient.m
@@ -8,26 +8,8 @@ function g = sigmoidGradient(z)
 
 g = zeros(size(z));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the gradient of the sigmoid function evaluated at
-%               each value of z (z can be a matrix, vector or scalar).
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% =============================================================
-
-
-
+% Calculate sigmoid gradient
+sig = sigmoid(z);
+g = sig .* (1 - sig);
 
 end
